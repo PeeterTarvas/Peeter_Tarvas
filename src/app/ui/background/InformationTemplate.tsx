36a27a3937e1220184d02d2a52115e0081fa4c14
Peeter Tarvas <peetertarvas@gmail.com>
2024-07-17 23:07:45 +0300

    add

--- src/app/ui/background/InformationTemplate.tsx
+++ src/app/ui/background/InformationTemplate.tsx
@@ -12,19 +12,19 @@ export interface InformationTemplateInterface {
 const getText = (text: Map<string, string[]>) => {
     const elements: React.JSX.Element[] = [];
     text.forEach((values, key) => {
-        elements.push(<ul className={"list-none mx-2 text-2xl"} key={key}>{key}</ul>);
+        elements.push(<ul className={"list-none mx-4 text-3xl"} key={key}>{key}</ul>);
         values.forEach((value, index) => {
-            elements.push(<li className={"mx-2 my-4 text-xl"} key={`${key}-${index}`}>{value}</li>);
+            elements.push(<li className={"ml-8 my-4 text-xl"} key={`${key}-${index}`}>{value}</li>);
         });
     });
     return elements;
 };
 
 const LeftTemplate = React.forwardRef<HTMLDivElement, InformationTemplateInterface>(({ title, text }, ref) => (
-        <div ref={ref} className="my-10 p-5 xl:p10 rounded-r-3xl
+        <div ref={ref} className="my-10 p-2 lg:p-6 rounded-3xl
             min-w-full max-w-full md:min-w-128 md:max-w-128
-            bg-regal-blue self-start text-white animate-rollInFromLeft shadow-lg">
-            <h1 className={"my-4 text-4xl"}>{title}</h1>
+            bg-regal-blue lg:ml-10 self-start text-white animate-rollInFromLeft shadow-lg">
+            <h1 className={"lg:mb-6 lg:mt-4 lg:text-5xl my-4 text-2xl"}>{title}</h1>
             <ul className={"list-none m-1 text-4xl"}>
                 {getText(text)}
             </ul>
@@ -33,10 +33,10 @@ const LeftTemplate = React.forwardRef<HTMLDivElement, InformationTemplateInterfa
 LeftTemplate.displayName = "LeftTemplate";
 
 const RightTemplate = React.forwardRef<HTMLDivElement, InformationTemplateInterface>(({ title, text }, ref) => (
-        <div ref={ref} className="my-10 p-5 xl:p10 rounded-l-3xl
+        <div ref={ref} className="my-10 p-2 lg:p-6 rounded-3xl
             min-w-full max-w-full md:min-w-128 md:max-w-128 max-h-96
-            bg-regal-blue self-end text-white animate-rollInFromRight drop-shadow-sm">
-            <h1 className={"my-4 text-4xl"}>{title}</h1>
+            bg-regal-blue lg:mr-10 self-end text-white animate-rollInFromRight drop-shadow-sm">
+            <h1 className={"lg:mb-6 lg:mt-4 lg:text-5xl my-4 text-2xl"}>{title}</h1>
             <ul className={"list-none m-1 text-4xl"}>
                 {getText(text)}
             </ul>
