23979005d1404c739199d43f4f74b060f3378405
peeter.tarvas <peetertarvas@gmail.com>
2024-08-23 23:23:52 +0300

    add new projects section

--- src/app/repository/ProjectRepository.tsx
+++ src/app/repository/ProjectRepository.tsx
@@ -1,12 +1,23 @@
+//const links = new Map<string, string[]>([
+//    ['Drawing Strategies Analysis for \n' +
+//    'Embedded Figure Drawing Tests',
+//        ['https://digikogu.taltech.ee/et/Item/270479c8-0f65-44b3-a4b2-4aec65e28d58', "../assets/thesis.png"]],
+//    ['BoxheadTypeGame', ['https://github.com/PeeterTarvas/BoxheadTypeGame', "../assets/boxhead.png"]],
+//    ['JavaTask', ['https://github.com/PeeterTarvas/JavaTask', "../assets/javatask.png"]],
+//    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "../assets/stockviewer.png"]],
+//    ['AntColonySimulator', ['https://github.com/PeeterTarvas/AntColonySimulator', "../assets/antcolony.png"]],
+//    ['CharningGrid', ['https://github.com/PeeterTarvas/ChargingGrid', "../assets/charginggrid.png"]],
+//]);
+
 const links = new Map<string, string[]>([
     ['Drawing Strategies Analysis for \n' +
     'Embedded Figure Drawing Tests',
-        ['https://digikogu.taltech.ee/et/Item/270479c8-0f65-44b3-a4b2-4aec65e28d58', "../assets/thesis.png"]],
-    ['BoxheadTypeGame', ['https://github.com/PeeterTarvas/BoxheadTypeGame', "../assets/boxhead.png"]],
-    ['JavaTask', ['https://github.com/PeeterTarvas/JavaTask', "../assets/javatask.png"]],
-    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "../assets/stockviewer.png"]],
-    ['AntColonySimulator', ['https://github.com/PeeterTarvas/AntColonySimulator', "../assets/antcolony.png"]],
-    ['CharningGrid', ['https://github.com/PeeterTarvas/ChargingGrid', "../assets/charginggrid.png"]],
+        ['https://digikogu.taltech.ee/et/Item/270479c8-0f65-44b3-a4b2-4aec65e28d58', "/assets/thesis.png"]],
+    ['BoxheadTypeGame', ['https://github.com/PeeterTarvas/BoxheadTypeGame', "/assets/boxhead.png"]],
+    ['JavaTask', ['https://github.com/PeeterTarvas/JavaTask', "/assets/javatask.png"]],
+    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "/assets/stockviewer.png"]],
+    ['AntColonySimulator', ['https://github.com/PeeterTarvas/AntColonySimulator', "/assets/antcolony.png"]],
+    ['CharningGrid', ['https://github.com/PeeterTarvas/ChargingGrid', "/assets/charginggrid.png"]],
 ]);
 
 
