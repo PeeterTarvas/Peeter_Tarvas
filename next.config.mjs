32d882900ec837b41a424d01e3b78f710add8aab
peeter.tarvas <peetertarvas@gmail.com>
2024-08-26 22:17:58 +0200

    update style

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         unoptimized: true,
-        loader: "akamai",
-        path: "",
     },
     assetPrefix: "/",
     webpack(config) {
