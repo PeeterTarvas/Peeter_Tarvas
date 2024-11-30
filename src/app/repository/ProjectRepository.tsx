5ee9569aa0acb8a7d79cb15971a0ef47971c86ed
peeter.tarvas <peetertarvas@gmail.com>
2024-11-30 23:18:08 +0100

    update style

--- src/app/repository/ProjectRepository.tsx
+++ src/app/repository/ProjectRepository.tsx
@@ -1,12 +1,12 @@
 const links = new Map<string, string[]>([
     ['Drawing Strategies Analysis for \n' +
     'Embedded Figure Drawing Tests',
-        ['https://digikogu.taltech.ee/et/Item/270479c8-0f65-44b3-a4b2-4aec65e28d58', "/static/images/thesis.png"]],
-    ['BoxheadTypeGame', ['https://github.com/PeeterTarvas/BoxheadTypeGame', "/static/images/boxhead.png"]],
-    ['JavaTask', ['https://github.com/PeeterTarvas/JavaTask', "/static/images/javatask.png"]],
-    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "/static/images/stockviewer.png"]],
-    ['AntColonySimulator', ['https://github.com/PeeterTarvas/AntColonySimulator', "/static/images/antcolony.png"]],
-    ['CharningGrid', ['https://github.com/PeeterTarvas/ChargingGrid', "/static/images/charginggrid.png"]],
+        ['https://digikogu.taltech.ee/et/Item/270479c8-0f65-44b3-a4b2-4aec65e28d58', "/images/thesis.png"]],
+    ['BoxheadTypeGame', ['https://github.com/PeeterTarvas/BoxheadTypeGame', "/images/boxhead.png"]],
+    ['JavaTask', ['https://github.com/PeeterTarvas/JavaTask', "/images/javatask.png"]],
+    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "/stockviewer.png"]],
+    ['AntColonySimulator', ['https://github.com/PeeterTarvas/AntColonySimulator', "/images/antcolony.png"]],
+    ['CharningGrid', ['https://github.com/PeeterTarvas/ChargingGrid', "/images/charginggrid.png"]],
 ]);
 
 
