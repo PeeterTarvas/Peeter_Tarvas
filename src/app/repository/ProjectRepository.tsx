ff87255a613843012727cbf89f4974b6140bbf59
peeter.tarvas <peetertarvas@gmail.com>
2024-11-30 23:32:11 +0100

    update style

--- src/app/repository/ProjectRepository.tsx
+++ src/app/repository/ProjectRepository.tsx
@@ -4,7 +4,7 @@ const links = new Map<string, string[]>([
         ['https://digikogu.taltech.ee/et/Item/270479c8-0f65-44b3-a4b2-4aec65e28d58', "/images/thesis.png"]],
     ['BoxheadTypeGame', ['https://github.com/PeeterTarvas/BoxheadTypeGame', "/images/boxhead.png"]],
     ['JavaTask', ['https://github.com/PeeterTarvas/JavaTask', "/images/javatask.png"]],
-    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "/stockviewer.png"]],
+    ['StockViewer', ['https://github.com/PeeterTarvas/StockViewer', "/images/stockviewer.png"]],
     ['AntColonySimulator', ['https://github.com/PeeterTarvas/AntColonySimulator', "/images/antcolony.png"]],
     ['CharningGrid', ['https://github.com/PeeterTarvas/ChargingGrid', "/images/charginggrid.png"]],
 ]);
